d3565ea0174ae90587e940a8b59f91fd8db66bf0
azadeh dehgan <azadeh69db@gmail.com>
2025-10-26 20:25:17 +0330

    ok

--- js/HomePage4.js
+++ js/HomePage4.js
@@ -1,24 +1,29 @@
-const btntodayEl=document.querySelector(".btnToday");
-const filterDivEl=document.querySelector(".filterDiv")
-const  TodayProductEl=document.querySelector(".TodayProduct");
-btntodayEl.addEventListener("click" , myFunction1);
-function myFunction1(){
-  TodayProductEl.style.backgroundColor="red";
+const btntodayEl = document.querySelector(".btnToday");
+const filterDivEl = document.querySelector(".filterDiv")
+const TodayProductEl = document.querySelector(".TodayProduct");
+btntodayEl.addEventListener("click", myFunction1);
+function myFunction1() {
+  TodayProductEl.style.backgroundColor = "red";
   filterDivEl.classList.remove("specialProduct");
+  filterDivEl.classList.remove("HomeProduct");
 }
 
-const btnSpecialEl=document.querySelector(".btnSpecial");
-const  specialProductEl=document.querySelector(".specialProduct");
-btnSpecialEl.addEventListener("click" , myFunction2);
-function myFunction2(){
- specialProductEl.style.backgroundColor="blue";
+const btnSpecialEl = document.querySelector(".btnSpecial");
+const specialProductEl = document.querySelector(".specialProduct");
+btnSpecialEl.addEventListener("click", myFunction2);
+function myFunction2() {
+  specialProductEl.style.backgroundColor = "blue";
   filterDivEl.classList.remove("TodayProduct");
+  filterDivEl.classList.remove("HomeProduct");
+
 }
 
-const btnHomeEl=document.querySelector(".btnHome");
-const  HomeProductEl=document.querySelector(".HomeProduct");
-btnHomeEl.addEventListener("click" , myFunction3);
-function myFunction3(){
- HomeProductEl.style.backgroundColor="green";
+const btnHomeEl = document.querySelector(".btnHome");
+const HomeProductEl = document.querySelector(".HomeProduct");
+btnHomeEl.addEventListener("click", myFunction3);
+function myFunction3() {
+  HomeProductEl.style.backgroundColor = "green";
   filterDivEl.classList.remove("TodayProduct");
+  filterDivEl.classList.remove("specialProduct");
+
 }
